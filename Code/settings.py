cb14870e3c41bea02b6646381c759ae3a6ae93ca
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-10-03 03:47:42 +0200

    reduce redundancy

--- Code/settings.py
+++ Code/settings.py
@@ -1,12 +1,14 @@
 # Select "GB" for House of Commons or "IT" for Italian Senate:
-DATASET = "IT"
+DATASET = "GB"
+
+PARLAMINT_DIR = f"./data/{DATASET}/ParlaMint/"
+PEOPLE_FILE = PARLAMINT_DIR + f"ParlaMint-{DATASET}-listPerson.xml"
+UTTERANCES_DIR = f"./data/{DATASET}/utterances/"
+EMBEDDINGS_DIR = f"./data/{DATASET}/embeddings/"
 
 if DATASET == "GB":
     MODEL = "bert-base-uncased"
-    PARLAMINT_DIR = "./data/ParlaMint/ParlaMint-GB.TEI/"
-    PEOPLE_FILE = PARLAMINT_DIR + "ParlaMint-GB-listPerson.xml"
-    UTTERANCES_DIR = "./data/utterances/"
-    EMBEDDINGS_DIR = "./data/embeddings/"
+
     YEARS = [str(y) for y in range(2015, 2023)]
     TOPICS = [
         "education",
@@ -23,10 +25,6 @@ if DATASET == "GB":
 
 elif DATASET == "IT":
     MODEL = "dbmdz/bert-base-italian-uncased"
-    PARLAMINT_DIR = "./data/ParlaMint/ParlaMint-IT.TEI/"
-    PEOPLE_FILE = PARLAMINT_DIR + "ParlaMint-IT-listPerson.xml"
-    UTTERANCES_DIR = "./data/utterances/"
-    EMBEDDINGS_DIR = "./data/embeddings/"
     YEARS = [str(y) for y in range(2013, 2023)]
     TOPICS = [
         "scuola",
