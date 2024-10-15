1f4571d959ec73e2aa9e153127b98a5d99298a0d
Giulio Cusenza <92781048+GiulioCusenza@users.noreply.github.com>
2024-10-15 20:45:10 +0200

    improve code

--- Code/plot_word_occurrences.py
+++ Code/plot_word_occurrences.py
@@ -2,20 +2,27 @@ from settings import *
 
 import matplotlib.pyplot as plt
 import math
+from string import punctuation
+import regex as re
 
 def count_occurrences(topic):
-    occurrences = list() # n occurrences each year
+    n_occurrences = 0
+    sessions = set()
+    yearly_occurrences = list()
     for year in YEARS:
         count = 0
         for entry in open(f"{UTTERANCES_DIR}/{year}/{topic}.tsv", encoding="utf-8"):
-            text = entry.split("\t")[3]
-            for word in text.split():
-                if topic in word:
+            session, _, _, text = entry.split("\t")
+            sessions.add(session)
+            for word in re.sub(f"[{punctuation}]", " ", text).split():
+                if topic == word:
                     count += 1
-        occurrences.append(count)
-    return occurrences
+        n_occurrences += count
+        yearly_occurrences.append(count)
+    return n_occurrences, len(sessions), yearly_occurrences
 
 def plot_occurrences():
+    print("topic,\toccurrences,\tsessions,\toccurrences/session")
     n_cols = 3
     fig, axs = plt.subplots(
         nrows=math.ceil((len(TOPICS) // n_cols)),
@@ -26,7 +33,9 @@ def plot_occurrences():
     for i, topic in enumerate(TOPICS):
         ax = axs[i // n_cols][i % n_cols]
         ax.set_title(topic)
-        ax.plot(YEARS, count_occurrences(topic), marker='o', linestyle=':', color='#aa0000')
+        n_occurrences, n_sessions, yearly_occurrences = count_occurrences(topic)
+        print(topic, n_occurrences, n_sessions, n_occurrences / n_sessions, sep="\t")
+        ax.plot(YEARS, yearly_occurrences, marker='o', linestyle=':', color='#aa0000')
     fig.tight_layout()
     plt.show()
 
